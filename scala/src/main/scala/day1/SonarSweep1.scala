daf578b76ca9ea3e55b522326b02e0c525a0f50d
Tomas Pinos <tomas.pinos@citrix.com>
2021-12-01 14:20:20 +0100

    Advent of code 2021 - Day 1 (in Scala).

---
+++ scala/src/main/scala/day1/SonarSweep1.scala
@@ -0,0 +1,24 @@
+package day1
+
+object SonarSweep1 {
+
+  def main(args: Array[String]): Unit = {
+    val values = readFile()
+
+    val values1 = values.slice(0, values.size - 1)
+    val values2 = values.slice(1, values.size)
+    val increases = values1.to(LazyList).zip(values2).count { case (u, v) => u < v }
+
+    println(increases)
+  }
+
+  private def readFile(): List[Int] = {
+    val path = SonarSweep1.getClass.getResource("input.txt").getPath
+    val bufferedSource = io.Source.fromFile(path)
+    val lines = (for (line <- bufferedSource.getLines()) yield line)
+      .map(s => s.toInt)
+      .toList
+    bufferedSource.close
+    lines
+  }
+}
---
+++ scala/src/main/scala/day1/SonarSweep2.scala
@@ -0,0 +1,29 @@
+package day1
+
+object SonarSweep2 {
+
+  def main(args: Array[String]): Unit = {
+    val values = readFile()
+
+    val values1 = values.slice(0, values.size - 3)
+    val values2 = values.slice(1, values.size - 2)
+    val values3 = values.slice(2, values.size - 1)
+    val values4 = values.slice(3, values.size)
+    val slidingWindows1 = (values1, values2, values3).zipped
+    val slidingWindows2 = (values2, values3, values4).zipped
+    val increases = slidingWindows1.zip(slidingWindows2)
+      .count { case ((a, b, c), (u, v, x)) => a + b + c < u + v + x }
+
+    println(increases)
+  }
+
+  private def readFile(): List[Int] = {
+    val path = SonarSweep2.getClass.getResource("input.txt").getPath
+    val bufferedSource = io.Source.fromFile(path)
+    val lines = (for (line <- bufferedSource.getLines()) yield line)
+      .map(s => s.toInt)
+      .toList
+    bufferedSource.close
+    lines
+  }
+}
