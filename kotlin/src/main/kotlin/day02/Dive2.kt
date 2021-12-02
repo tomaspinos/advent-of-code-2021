2473f844c1818e00e8bf40f12008b9bebf9ea212
Tomas Pinos <tomas.pinos@citrix.com>
2021-12-02 14:11:18 +0100

    Advent of code 2021 - Day 2 (in Kotlin).

--- kotlin/src/main/kotlin/day02/Dive2.kt
+++ kotlin/src/main/kotlin/day02/Dive2.kt
@@ -4,32 +4,33 @@ import java.io.File
 
 fun main() {
     val instructions = readInput()
-    val position = Dive1().dive(instructions)
+    val position = Dive2().dive(instructions)
     println(position)
     println(position.horizontal * position.depth)
 }
 
-class Dive1 {
+class Dive2 {
 
     fun dive(instructions: List<String>): Position {
-        var position = Position(0, 0)
+        var position = Position(0, 0, 0)
 
         for (instruction in instructions) {
             val split = instruction.split(" ")
             val direction = split[0]
             val difference = split[1].toInt()
             when (direction) {
-                "forward" -> position = Position(position.horizontal + difference, position.depth)
-                "down" -> position = Position(position.horizontal, position.depth + difference)
-                "up" -> position = Position(position.horizontal, position.depth - difference)
+                "forward" -> position = Position(position.horizontal + difference, position.depth + (position.aim * difference), position.aim)
+                "down" -> position = Position(position.horizontal, position.depth, position.aim + difference)
+                "up" -> position = Position(position.horizontal, position.depth, position.aim - difference)
             }
         }
 
         return position
     }
 
-    data class Position(val horizontal: Int, val depth: Int)
+    data class Position(val horizontal: Int, val depth: Int, val aim: Int)
 }
+
 
 private fun readInput(): List<String> {
     return File(Dive1::class.java.getResource("input.txt").path)
--- kotlin/src/test/kotlin/day02/Dive2Test.kt
+++ kotlin/src/test/kotlin/day02/Dive2Test.kt
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.io.File
 
-internal class Dive1Test {
+internal class Dive2Test {
 
     @Test
     fun dive() {
         val instructions = readInput()
-        val position = Dive1().dive(instructions)
-        assertEquals(Dive1.Position(15, 10), position)
+        val position = Dive2().dive(instructions)
+        assertEquals(Dive2.Position(15, 60, 10), position)
     }
 
     private fun readInput(): List<String> {
