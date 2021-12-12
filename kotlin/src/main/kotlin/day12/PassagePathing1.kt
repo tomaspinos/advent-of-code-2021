6dca61e83f6c3aa8a0b3250ca88288a92b262bc6
Tomas Pinos <tomas.pinos@citrix.com>
2021-12-12 18:36:46 +0100

    Advent of code 2021 - Day 12 (in Kotlin).

--- kotlin/src/main/kotlin/day12/PassagePathing1.kt
+++ kotlin/src/main/kotlin/day12/PassagePathing1.kt
@@ -3,9 +3,7 @@ package day12
 import Common
 
 fun main() {
-    val input = Common.readInput("/day12/input.txt")
-    val result = PassagePathing1().paths(input)
-    println(result)
+    println(PassagePathing1().paths(Common.readInput("/day12/input.txt")))
 }
 
 class PassagePathing1 {
@@ -15,9 +13,7 @@ class PassagePathing1 {
     private val endNode: Node = Node("end")
 
     fun paths(input: List<String>): Int {
-        val steps = readSteps(input)
-        val startingPath = Path(listOf(startNode))
-        val paths = generatePaths(startingPath, steps)
+        val paths = generatePaths(Path(listOf(startNode)), readSteps(input))
         println("paths: $paths")
         return paths.size
     }
@@ -35,22 +31,19 @@ class PassagePathing1 {
     }
 
     private fun generatePaths(path: Path, steps: Steps): List<Path> {
-        println("generatePaths($path, $steps)")
-        val possibleSteps = steps.getPossibleSteps(path.lastNode())
-        val resultingPaths = ArrayList<Path>()
-        for (possibleStep in possibleSteps) {
-            if (possibleStep.to == endNode) {
-                resultingPaths.add(path.append(possibleStep.to))
-            } else {
-                val remainingSteps = if (possibleStep.to.canBeVisitedOnlyOnce) {
-                    steps.removeStepsLeadingTo(possibleStep.to)
+        return steps.getPossibleSteps(path.lastNode())
+            .flatMap { possibleStep ->
+                if (possibleStep.to == endNode) {
+                    listOf(path.append(possibleStep.to))
                 } else {
-                    steps
+                    val remainingSteps = if (possibleStep.to.canBeVisitedOnlyOnce) {
+                        steps.removeStepsLeadingTo(possibleStep.to)
+                    } else {
+                        steps
+                    }
+                    generatePaths(path.append(possibleStep.to), remainingSteps)
                 }
-                resultingPaths.addAll(generatePaths(path.append(possibleStep.to), remainingSteps))
             }
-        }
-        return resultingPaths
     }
 
     data class Node(val name: String) {
--- kotlin/src/main/kotlin/day12/PassagePathing2.kt
+++ kotlin/src/main/kotlin/day12/PassagePathing2.kt
@@ -3,9 +3,7 @@ package day12
 import Common
 
 fun main() {
-    val input = Common.readInput("/day12/input.txt")
-    val result = PassagePathing2().paths(input)
-    println(result)
+    println(PassagePathing2().paths(Common.readInput("/day12/input.txt")))
 }
 
 class PassagePathing2 {
@@ -15,9 +13,7 @@ class PassagePathing2 {
     private val endNode: Node = Node("end")
 
     fun paths(input: List<String>): Int {
-        val steps = readSteps(input)
-        val startingPath = Path(listOf(startNode))
-        val paths = generatePaths(startingPath, steps)
+        val paths = generatePaths(Path(listOf(startNode)), readSteps(input))
         println("paths: $paths")
         return paths.size
     }
@@ -35,17 +31,16 @@ class PassagePathing2 {
     }
 
     private fun generatePaths(path: Path, steps: Steps): List<Path> {
-        println("generatePaths($path, $steps)")
-        val possibleSteps = steps.getPossibleSteps(path.lastNode())
-        val resultingPaths = ArrayList<Path>()
-        for (possibleStep in possibleSteps) {
-            if (possibleStep.to == endNode) {
-                resultingPaths.add(path.append(possibleStep.to))
-            } else if (path.canAppend(possibleStep.to)) {
-                resultingPaths.addAll(generatePaths(path.append(possibleStep.to), steps))
+        return steps.getPossibleSteps(path.lastNode())
+            .flatMap { possibleStep ->
+                if (possibleStep.to == endNode) {
+                    listOf(path.append(possibleStep.to))
+                } else if (path.canAppend(possibleStep.to)) {
+                    generatePaths(path.append(possibleStep.to), steps)
+                } else {
+                    emptyList()
+                }
             }
-        }
-        return resultingPaths
     }
 
     data class Node(val name: String) {
